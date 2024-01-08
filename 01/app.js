45984c732f9d651925054459bc4b6d0438a0f7b0
Agnieszka Kaminska <zabula81@o2.pl>
2024-01-08 14:45:10 +0100

    Frist-1

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,11 @@
-const submit = document.querySelector("input[type=submit]");
-
-const fristName = document.querySelector('[name="firstName"]');
-const lastName = document.querySelector('[name="lastName"]');
-
+const form = document.querySelector("form");
 const list = document.querySelector("ul");
 
-submit.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const name = fristName.value;
-  const surname = lastName.value;
+  let name = form.firstName.value;
+  let surname = form.lastName.value;
 
   let listEl = document.createElement("li");
   listEl.innerHTML = `${name} ${surname}`;
